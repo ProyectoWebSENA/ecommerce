94b2ae1f4eaa2fe745cf1371e8a20172f336d40a
Juan Rojo <jjrojoa@gmail.com>
2022-10-25 19:29:53 -0500

    Inicializacion proyecto. Creación punto de entrada y conexicon a db.

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+define('URL', 'http://localhost:80/ecommerce');
+
+define('HOST', 'localhost');
+define('DB', 'ecommerce');
+define('USER', 'root');
+define('PASSWORD', '');
+define('CHARSET', 'uft8mb4');
---
+++ libs/app.php
@@ -0,0 +1,54 @@
+<?php
+
+//require_once 'controllers/errores.php';
+
+
+class App
+{
+  function __construct()
+  {
+    $url = isset($_GET['url']) ? $_GET['url'] : null;
+
+    if (empty($url)) {
+      $archController = 'controllers/homeController.php';
+      require_once $archController;
+      $controller = new HomeController();
+      $controller->loadModel('home');
+      $controller->render();
+      return false;
+    }
+
+    $url = rtrim($url, '/');
+    $url = explode('/', $url);
+
+    $archController = 'controllers' . $url[0] . 'Controller.php';
+
+    if (file_exists($archController)) {
+      require_once $archController;
+      $controller = new $url[0];
+      $controller->loadModel($url[0]);
+      if (isset($url[1])) {
+        if (method_exists($controller, $url[1])) {
+          if (isset($url[2])) {
+            $nparam = count($url) - 2;
+            $params = [];
+
+            for ($i = 0; $i < $nparam; $i++) {
+              array_push($params, $url[$i] + 2);
+            }
+
+            $controller->{$url[1]($params)};
+          } else {
+            $controller->{$url[1]()};
+          }
+        } else {
+          //No existe el metodo -> 404
+        }
+      } else {
+        $controller->render();
+      }
+    } else {
+      //No existe el controlador -> 404
+    }
+  }
+}
