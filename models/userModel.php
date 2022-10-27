b507a13fc276e33065d347cda4ace0b35d13896c
esteifer <esteifer.montoya.censa@gmail.com>
2022-10-27 17:48:33 -0500

    Inicializado el constructor del modelo de usuario.

--- models/userModel.php
+++ models/userModel.php
@@ -2,6 +2,28 @@
 
 class UserModel extends Model implements IModel
 {
+  private $id;
+  private $name;
+  private $username;
+  private $email;
+  private $cellphone;
+  private $address;
+  private $hash_password;
+  private $pic_url;
+   
+  public function __construct()
+  {
+    parent ::__construct();
+    $this->id ="";
+    $this->name ="";
+    $this->username ="";
+    $this->email ="";
+    $this->cellphone =0;
+    $this->address ="";
+    $this->hash_password ="";
+    $this->pic_url ="";
+  }
+
   public function save()
   {
   }
