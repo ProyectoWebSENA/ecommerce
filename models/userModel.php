67b3704e05bf3edc4a45bd4b750c2b0447c894a8
esteifer <esteifer.montoya.censa@gmail.com>
2022-11-02 18:16:17 -0500

    Creados metodos setters y getters

--- models/userModel.php
+++ models/userModel.php
@@ -39,4 +39,24 @@ class UserModel extends Model implements IModel
   public function from($array)
   {
   }
+
+  public function setId($id) { $this -> id = $id;}
+  public function seName($name) { $this -> name = $name;}
+  public function setUsername($username) { $this -> username = $username;}
+  public function setEmail($email) { $this -> email = $email;}
+  public function setCellphone($cellphone) { $this -> cellphone = $cellphone;}
+  public function setAdrees($address) { $this -> address = $address;}
+  public function setPassword($hash_password) { $this -> hash_password = $hash_password;}
+  public function setPic($pic_url) { $this -> pic_url = $pic_url;} 
+
+
+
+  public function getId() { $this -> id ;}
+  public function getName() { $this -> name ;}
+  public function getUsername() { $this -> username;}
+  public function getEmail() { $this -> email ;}
+  public function getCellphone() { $this -> cellphone ;}
+  public function getAdrees() { $this -> address ;}
+  public function getPassword() { $this -> hash_password ;}
+  public function getPic() { $this -> pic_url ;}
 }
