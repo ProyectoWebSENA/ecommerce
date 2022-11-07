8c3fb824ee506ee8bccd510f27a86048b0c86bc3
Juan Rojo <jjrojoa@gmail.com>
2022-11-07 18:47:55 -0500

    Arreglados errores de sintaxis

--- controllers/signupController.php
+++ controllers/signupController.php
@@ -1,27 +1,24 @@
 <?php
 
-class signupController extends SessionController
+class SignupController extends SessionController
 {
-    function __construct()
-    {
-        parent:: __construct();
-    }
- function render(){
-    $this -> view -> render('login/singup');
- }
- 
- function newUser(){
-    if($this-> existsPOST(['name', 'username','email', 'cellphone',
-    'address'
-    ,'hash_pasword', 'pic_url', 'role'
-    ])){
-        $name = $this->getPost('name');
-        $username= $this->getPost('username');
-        $email = $this->getPost('email');
-        $address = $this->getPost('address');
-        $password = $this->getPost('password');
-        $pic_url= $this->getPost('pic_url');
-        $role = $this->getPost('role');
+  function __construct()
+  {
+    parent::__construct();
+  }
+  function render()
+  {
+    $this->view->render('login/signup');
+  }
+
+  function newUser()
+  {
+    if ($this->existsPOST(['name', 'email', 'cellphone', 'address', 'hash_pasword', 'role'])) {
+      $name = $this->getPost('name');
+      $email = $this->getPost('email');
+      $address = $this->getPost('address');
+      $password = $this->getPost('password');
+      $role = $this->getPost('role');
     }
   }
 }
